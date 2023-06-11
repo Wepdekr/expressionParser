b2118d78226a475c1c4d481115820f0c16729c53
Wepdekr <1013006824@qq.com>
2023-06-11 22:42:55 +0800

    updated syntacticParser.py

--- syntacticParser.py
+++ syntacticParser.py
@@ -5,10 +5,20 @@ class syntacticParser:
 
     class node:
         def __init__(self):
-            self.fa = ''
             self.son = []
             self.val = ''
             self.type = ''
+        
+        def __init__(self, val, type):
+            self.son = []
+            self.val = val
+            self.type = type
+        
+        def getVal(self):
+            return self.val
+        
+        def addSon(self, s):
+            self.son.append(s)
 
     def getRevPolish(self, VtList):
         stack = []
@@ -48,9 +58,35 @@ class syntacticParser:
         
         return ret
 
+    def getReal(intSec, decSec):
+        real = str(intSec) + '.' + str(decSec)
+        return float(real)
+
     def buildTree(self, revPolish):
+        stackE = []
+        stackT = []
         for i in revPolish:
-            pass
+            if i.isdigit() and stackE[-1] == '.':
+                dot = stackT.pop()
+                intSec = stackT.pop()
+                decSec = self.node(val=int(i), type='digit')
+                real = self.getReal(intSec=intSec.getVal(), decSec=decSec.getVal())
+                n = self.node(val=real, type='real')
+                n.addSon(intSec)
+                n.addSon(dot)
+                n.addSon(decSec)
+                stackT.append(n)
+                stackE.pop()
+                stackE.pop()
+                stackE.append(real)
+            elif i.isdigit():
+                stackE.append(i)
+                stackT.append(self.node(val=int(i), type='digit'))
+            elif i == ')':
+                stackE.append(i)
+                stackT.append(self.node(val=i, type='rightparen'))
+            elif i == '+':
+                pass
 
     def parser(self, VtList):
         revPolish = self.getRevPolish(VtList=VtList)
