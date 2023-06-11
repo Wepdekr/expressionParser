b71b55a40204fabb92896a8ec8b3caec4afe8819
Wepdekr <1013006824@qq.com>
2023-06-11 21:37:57 +0800

    modified getRevPolish in syntacticParser.py

--- syntacticParser.py
+++ syntacticParser.py
@@ -1,36 +1,42 @@
-def parser(VtList):
-    # print(VtList)
+def getRevPolish(VtList):
     stack = []
-    revPolish = []
+    ret = []
     for i in VtList:
         if i.isdigit() or i == '.' or i.isalpha():
-            revPolish.append(i)
+            ret.append(i)
         elif i == '(':
-            revPolish.append(')')
+            ret.append(')')
             stack.append('(')
         else:
             if i == ')':
                 while len(stack) > 0 and stack[-1] != '(':
-                    revPolish.append(stack[-1])
+                    ret.append(stack[-1])
                     stack.pop()
-                revPolish.append('(')
+                ret.append('(')
                 stack.pop()
             elif i == '+' or i == '-':
                 while len(stack) > 0 and stack[-1] != '(':
-                    revPolish.append(stack[-1])
+                    ret.append(stack[-1])
                     stack.pop()
                 stack.append(i)
             elif i == '*' or i == '/':
                 while len(stack) > 0 and (stack[-1] == '+' or stack[-1] == '-'):
-                    revPolish.append(stack[-1])
+                    ret.append(stack[-1])
                     stack.pop()
                 stack.append(i)
             else:
                 while len(stack) > 0 and stack[-1] != '^' and stack[-1] != '(':
-                    revPolish.append(stack[-1])
+                    ret.append(stack[-1])
                     stack.pop()
                 stack.append(i)
     
     while len(stack) > 0:
+        ret.append(stack[-1])
         stack.pop()
+    
+    return ret
+
+def parser(VtList):
+    revPolish = getRevPolish(VtList=VtList)
     print(revPolish)
+    
